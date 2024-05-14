c9d190d8e92f37e11632cca60627d8680f00b7a8
GaoXiong <xiong.gao@shopee.com>
2024-05-14 17:52:38 +0800

    chore: dev

--- stock-cli/src/b.ts
+++ stock-cli/src/b.ts
@@ -1,23 +1,30 @@
-import fs from 'fs'
-import * as cheerio from 'cheerio';
+// import fs from 'fs'
+// import * as cheerio from 'cheerio';
 
-const content = fs.readFileSync('./a.html', 'utf-8');
+// const content = fs.readFileSync('./a.html', 'utf-8');
 
-const $ = cheerio.load(content);
+// const $ = cheerio.load(content);
 
-$('.box table').each((i: any, el: any) => {
-  if (i === 0) {
-    $(el).find("tr").each((j: any, el) => {
-      if (j < 30) {
-        const item = {
-          code: $(el.children[1]).text(),
-          title: $(el.children[2]).text(),
-          ratio: $(el.children[6]).text(),
-          count: $(el.children[7]).text(),
-          money: ($(el.children[8]).text() || '0').replaceAll(',', ''),
-        }
-        console.log(j, item)
-      }
-    })
-  }
-})
+// $('.box table').each((i: any, el: any) => {
+//   if (i === 0) {
+//     $(el).find("tr").each((j: any, el) => {
+//       if (j < 30) {
+//         const item = {
+//           code: $(el.children[1]).text(),
+//           title: $(el.children[2]).text(),
+//           ratio: $(el.children[6]).text(),
+//           count: $(el.children[7]).text(),
+//           money: ($(el.children[8]).text() || '0').replaceAll(',', ''),
+//         }
+//         console.log(j, item)
+//       }
+//     })
+//   }
+// })
+
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc)
+
+console.log(dayjs(1715699039000).utc().local().format())
